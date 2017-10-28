5483771a38b9505cd72a00631a9e132706616d9f
ilya682003 <ilyanfs682003@mail.ru>
2017-10-28 16:57:04 +0300

    Массивы

--- menu.php
+++ menu.php
@@ -3,4 +3,6 @@
     <li role="presentation"><a href="summa.php"><span class="glyphicon glyphicon-plus" aria-hidden="true"></span> Сумма чисел</a></li>
     <li role="presentation"><a href="kosti.php"><span class="glyphicon glyphicon-unchecked" aria-hidden="true"></span> Игра в кости</a></li>
     <li role="presentation"><a href="http://vk.com"><span class="glyphicon glyphicon-send" aria-hidden="true"></span> Вконтакте</a></li>
+    <li role="presentation" class="active"><a href="contacts.php"><span class="glyphicon glyphicon-th" aria-hidden="true"></span>Контакты</a></li>
+    <li role="presentation" class="active"><a href="arrays.php"><span class="glyphicon glyphicon-th" aria-hidden="true"></span>Массивы</a></li>
 </ul>

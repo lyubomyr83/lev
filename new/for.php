e95b81566be20e76014883193915aa4a15ef27b9
1 <1@2.com>
2018-10-13 17:43:35 +0300

    new project inside lev

---
+++ new/for.php
@@ -0,0 +1,14 @@
+<?php
+require_once "header.php";
+
+
+for($i=1;$i<=20;$i++)
+{
+    if($i%2==0)
+    {
+        echo "Строка ".$i."<br> ";
+    }
+
+}
+
+require_once "footer.php";
---
+++ new/index.php
@@ -0,0 +1,9 @@
+<?php
+
+require_once "header.php";
+?>
+<img src ="img/1.jpg" width="350px" height="200px">;
+<?php
+require_once "footer.php";
+
+
